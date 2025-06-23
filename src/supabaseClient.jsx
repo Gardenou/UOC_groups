ff0fc568d7983fde7c2112c32cc735d73535dee6
Jordi Garriga <jgarrigamoon@uoc.edu>
2025-06-23 16:45:01 +0200

    Ocultar tokens SupaBase

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wxjvolyzotdojeevandn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4anZvbHl6b3Rkb2plZXZhbmRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NTMzOTUsImV4cCI6MjA2NjEyOTM5NX0.ylAKYZPnH6UH1x3bJVqGg3zzeb_PEgVTQ4PhxO2D0dQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
