f68a52a0b9c166e48ff7b63a907bb4631bc0a782
Jordi Garriga <jgarrigamoon@uoc.edu>
2025-06-25 01:05:50 +0200

    prova

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,3 @@
-/*import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./Navbar";
-import ItinerariPage from "./ItinerariPage";
-import { itinerarisData } from "./itineraris/index.jsx";
-
-export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/basiques" replace />} />
-        {Object.entries(itinerarisData).map(([slug, config]) => (
-          <Route
-            key={slug}
-            path={`/${slug}`}
-            element={<ItinerariPage bg={config.bg} assignatures={config.assignatures} />}
-          />
-        ))}
-      </Routes>
-    </Router>
-  );
-}*/
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -39,6 +16,7 @@ function AppRoutes() {
         {Object.entries(itinerarisData).map(([slug, config]) => {
           // Substitueix -ca per -es si cal
           const bg = language === "ca" ? config.bg : config.bg.replace("_ca", "_es");
+          console.log("Background image path:", bgPath);
 
           return (
             <Route
