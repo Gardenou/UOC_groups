b0fb42205371623538b4ac51bf1a87d961b70b96
Jordi Garriga <jgarrigamoon@uoc.edu>
2025-06-24 18:44:46 +0200

    afegir cmputadors

--- src/itineraris/computadors.jsx
+++ src/itineraris/computadors.jsx
@@ -2,10 +2,120 @@ import bg from "../../public/computadors_bg.png";
 
 const assignatures = [
 {
-    "nom": "ALG",
+    "nom": "XAI",
     "descripcio": "Descripció...",
-    "telegram": "https://t.me/+Tn4Ql3wROm9kYmI0",
-    "plaDocent": "https://apps.uoc.edu/PlaDocent/PlaDocent?Semestre=20251&SignatureCode=05.557&Context=2&Locale=ca", 
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "PP",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "SSOO",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "SSDD",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "ESTRAUCTURA XARXES COMPUTADORS",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "SISTENES ENCASTATS",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "AXSO",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "DSSOO",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "ARQUITECTURA DE COMPUTADORS",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "SEGURETAT XARXES COMPUTADORS",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "ARQUITECTURA COMPUTADORS AVANÇADES",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
+    "top": "12.00%",
+    "left": "4.30%",
+    "width": "11.00%",
+    "height": "7.5%"
+},
+{
+    "nom": "TFG",
+    "descripcio": "Descripció...",
+    "telegram": "",
+    "plaDocent": "", 
     "top": "12.00%",
     "left": "4.30%",
     "width": "11.00%",
